e9b20e896a3605cabf5b8e74629b2c7ca944cf2b
Andrew Weisman <andrew.weisman@nih.gov>
2021-05-17 18:22:12 -0400

    Add tox.ini for flake8 and pycodestyle settings; update wrapper scripts to utilize command line arguments to canonically-CANDLE-compliant scripts, including candle_compliant_wrapper.py

--- commands/submit-job/candle_compliant_wrapper.py
+++ commands/submit-job/candle_compliant_wrapper.py
@@ -1,15 +1,13 @@
 # This file should follow the current standard CANDLE-compliance procedure (what I'm calling to be "canonically CANDLE-compliant" to contrast it with the easier "CANDLE-compliance" that these wrapper scripts enable)
 
-def initialize_parameters():
-
-    # Import relevant libraries
-    import os # note "candle" is put in the path by the lmod module file
+def initialize_parameters(default_model='dummy_file_to_never_be_used.txt'):
 
     # Set variables from environment variables
-    dl_backend = os.getenv('CANDLE_DL_BACKEND') # set in input file (and checked and exported in preprocess.py)
-    default_model = os.getenv('CANDLE_DEFAULT_MODEL_FILE') # set in submit-job/command_script.sh... note that this is needed here for running in interactive mode, where default_model is NOT automatically set in model_runner.py per Justin's recent ~5/13/21 fix
-    desc = os.getenv('CANDLE_MODEL_DESCRIPTION') # set in run_workflows.sh
-    prog_name = os.getenv('CANDLE_PROG_NAME') # set in run_workflows.sh
+    import os
+    dl_backend = os.getenv('CANDLE_DL_BACKEND')  # set in input file (and checked and exported in preprocess.py)
+    # default_model = os.getenv('CANDLE_DEFAULT_MODEL_FILE') # set in submit-job/command_script.sh... note that this is needed here for running in interactive mode, where default_model is NOT automatically set in model_runner.py per Justin's recent ~5/13/21 fix... actually hoping to fix this now by adding --config_file to the script argument in interactive mode, so commenting this out again (5/17/21)
+    desc = os.getenv('CANDLE_MODEL_DESCRIPTION')  # set in run_workflows.sh
+    prog_name = os.getenv('CANDLE_PROG_NAME')  # set in run_workflows.sh
 
     # Adding this block on 5/15/21 because now, once candle is imported (right after this block), if a backend has not yet been imported, then $CANDLE/Benchmarks/common/candle/__init__.py will die with "No backend has been specified."
     # This doesn't matter for canonically CANDLE-compliant scripts because it is assumed that keras or pytorch have been imported at the top of these .py files.
@@ -20,15 +18,14 @@ def initialize_parameters():
     elif dl_backend == 'pytorch':
         import torch
 
-    import candle # note "candle" is put in the path by the lmod module file
-
     # Build benchmark object
+    import candle  # note "candle" is put in the path by the lmod module file
     myBmk = candle.Benchmark(os.path.dirname(os.path.realpath(__file__)), default_model, dl_backend, prog=prog_name, desc=desc)
 
     # Initialize parameters
     gParameters = candle.finalize_parameters(myBmk)
 
-    return(gParameters)
+    return gParameters
 
 
 def run(params):
@@ -49,9 +46,10 @@ def run(params):
 
     # Run the wrapper script model_wrapper.sh where the environment is defined and the model (whether in Python or R) is called
     with open('subprocess_out_and_err.txt', 'w') as myfile:
-        import subprocess, os
+        import subprocess
+        import os
         print('Starting run of model_wrapper.sh from candle_compliant_wrapper.py...')
-        subprocess.run(['bash', os.getenv('CANDLE')+'/wrappers/commands/submit-job/model_wrapper.sh'], stdout=myfile, stderr=subprocess.STDOUT)
+        subprocess.run(['bash', os.getenv('CANDLE') + '/wrappers/commands/submit-job/model_wrapper.sh'], stdout=myfile, stderr=subprocess.STDOUT)
         print('Finished run of model_wrapper.sh from candle_compliant_wrapper.py')
 
     # Read in the history.history dictionary containing the result from the JSON file created by the model
@@ -79,9 +77,9 @@ def main():
 if __name__ == '__main__':
     main()
     import os
-    if os.getenv('CANDLE_DL_BACKEND') == 'keras': # it could instead be "pytorch"
+    if os.getenv('CANDLE_DL_BACKEND') == 'keras':  # it could instead be "pytorch"
         from tensorflow.keras import backend as K
         try:
             K.clear_session()
-        except AttributeError: # theano does not have this function
+        except AttributeError:  # theano does not have this function
             pass
